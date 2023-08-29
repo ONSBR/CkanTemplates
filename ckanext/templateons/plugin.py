979eeb452eca53847f4b2f21bda090b6c4adebd8
jpbbgp <70491129+jpbbgp@users.noreply.github.com>
2023-08-29 12:20:46 -0300

    Update plugin.py with assets

--- ckanext/templateons/plugin.py
+++ ckanext/templateons/plugin.py
@@ -4,11 +4,11 @@ import ckan.plugins.toolkit as toolkit
 
 class TemplateonsPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-
+    
     # IConfigurer
 
     def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
-            'templateons')
+        toolkit.add_template_directory(config, 'templates')
+        toolkit.add_public_directory(config, 'public')
+        toolkit.add_resource('assets', 'ckanext-templateons')
+    
